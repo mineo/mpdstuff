057e04522c5c6a3d26272bbab75e5a316ae981e0
Wieland Hoffmann <the_mineo@web.de>
2010-03-15 19:10:12 +0100

    tabs -> spaces

--- xchat-mpd.py
+++ xchat-mpd.py
@@ -1,7 +1,7 @@
 __module_name__ ="MPD announcer"
 __module_version__ = "0.1"
 __module_decription__ = "Well, displays MPD info in a channel. \
-		Customizable by editing the script"
+        Customizable by editing the script"
 __module_author__ = "Wieland Hoffmann <themineo@gmail.com>"
 
 import xchat
@@ -9,9 +9,9 @@ from os import getenv
 
 """ Set up the basic stuff """
 try:
-	import mpd
+    import mpd
 except:
-	xchat.prnt("Couldn't load the mpd module")
+    xchat.prnt("Couldn't load the mpd module")
 
 mpd_host = getenv("MPD_HOST","localhost")
 mpd_port = getenv("MPD_PORT","6600")
@@ -21,28 +21,28 @@ mpd_pass = None
 client = mpd.MPDClient()
 client.connect(mpd_host,mpd_port)
 if mpd_pass:
-	client.password(mpd_pass)
+    client.password(mpd_pass)
 
 def announce_cb(word, word_eol, userdata):
-	global client
-	channel = xchat.get_info("channel")
-	try:
-		song = client.currentsong()
-	except mpd.ConnectionError,e:
-		xchat.prnt("Connection error %s" % e)
-		return xchat.EAT_ALL
-	album = song["album"]
-	""" Well, now follows the part which you have to customize for \
-		a different output """
-	title = song["title"]
-	artist = song["artist"]
-	xchat.command("msg %s np: %s - %s [%s]" \
-		% (channel, artist, title, album))
-	return xchat.EAT_ALL
+    global client
+    channel = xchat.get_info("channel")
+    try:
+        song = client.currentsong()
+    except mpd.ConnectionError,e:
+        xchat.prnt("Connection error %s" % e)
+        return xchat.EAT_ALL
+    album = song["album"]
+    """ Well, now follows the part which you have to customize for \
+        a different output """
+    title = song["title"]
+    artist = song["artist"]
+    xchat.command("msg %s np: %s - %s [%s]" \
+        % (channel, artist, title, album))
+    return xchat.EAT_ALL
 
 def unload_cb(userdata):
-	global client
-	client.disconnect()
+    global client
+    client.disconnect()
 
 
 xchat.hook_unload(unload_cb)
