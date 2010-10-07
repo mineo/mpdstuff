7e509ad0ac5fecc1e263fd7c35fcfeda1ea0b0c1
Wieland Hoffmann <themineo@gmail.com>
2010-10-07 16:47:35 +0200

    Add connect_via_env() to automatically connect when MPD_HOST and MPD_PORT are set

--- mpdext.py
+++ mpdext.py
@@ -1,4 +1,5 @@
 import mpd
+import os
 
 class MPDClientExt(mpd.MPDClient):
     def files_in_dir(self,dir):
@@ -17,3 +18,8 @@ class MPDClientExt(mpd.MPDClient):
                 print file
                 playlist_file.write(file+'\n')
         playlist_file.close()
+
+    def connect_via_env(self):
+        host = os.getenv("MPD_HOST","localhost")
+        port = os.getenv("MPD_PORT",6600)
+        self.connect(host,port)
